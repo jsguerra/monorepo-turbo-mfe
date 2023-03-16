fb7350d8ea35f5f3d66731234dfd5e94e39a09ca
jsguerra <jose@gcostudios.com>
2023-03-16 17:58:28 +0000

    imported fetch component

--- apps/blog/pages/blog.tsx
+++ apps/blog/pages/blog.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import FetchItems from "../components/FetchItems";
 
 export default function blog() {
   return <div>
@@ -6,5 +7,8 @@ export default function blog() {
     <Link href="/">Home</Link>
     <p>This page is generated from the Blog App and dynamically imported into Frontend blog page.</p>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo aperiam, ut inventore rerum corrupti totam praesentium error quasi rem, eaque, molestias saepe in illum animi deleniti eos magnam dolorum dolorem?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque asperiores magnam iste optio, distinctio quos debitis tempore maiores rem maxime aspernatur, veniam sapiente iusto? Nobis nostrum voluptas ut perspiciatis vero!</p>
+    <h2>The data below is fetched from a remote API</h2>
+    <FetchItems />
   </div>;
 }
