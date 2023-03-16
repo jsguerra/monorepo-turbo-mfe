f12968e856115412b6d09545e25739ae0e187d58
jsguerra <jose@gcostudios.com>
2023-03-16 17:35:28 +0000

    updated config

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,4 +1,22 @@
+const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
+
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["design-system"],
+  webpack: (config, options) => {
+    const { isServer } = options;
+    (config.experiments = { layers: true, topLevelAwait: true }),
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: "frontend",
+          remotes: {
+            blog: `blog@http://localhost:3001/_next/static/${
+              isServer ? "ssr" : "chunks"
+            }/remoteEntry.js`,
+          },
+          filename: "static/chunks/remoteEntry.js",
+        })
+      );
+    return config;
+  },
 };
