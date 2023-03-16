afab07aaf93f67fca4e8867f489738d407459e8a
jsguerra <jose@gcostudios.com>
2023-03-16 17:35:38 +0000

    minor update

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,10 +1,14 @@
 import { Button } from "design-system";
+import Link from "next/link";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
-      <Button />
+      <Link href="/blog">Blog</Link>
+      <p>
+        <Button />
+      </p>
     </div>
   );
 }
