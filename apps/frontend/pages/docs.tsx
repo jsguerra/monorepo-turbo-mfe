6e6ccca3ee7f10c4e25585dfe9d761fbed86e3f0
jsguerra <jose@gcostudios.com>
2023-03-19 23:39:29 +0000

    removed unused code

--- apps/frontend/pages/docs.tsx
+++ apps/frontend/pages/docs.tsx
@@ -1,9 +1,3 @@
-// import dynamic from "next/dynamic";
-
-// const Blog = dynamic(() => import("blog/pages/blog"), { ssr: false });
-
-// export default Blog;
-
 import dynamic from "next/dynamic";
 import Layout from "../components/layout/Layout";
 
